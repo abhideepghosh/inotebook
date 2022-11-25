8672fdc18a9ec4f2e306e4b079915e35936dc9fd
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-11-25 13:13:37 +0530

    MongoDB Setup

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
-});
+router.post(
+  "/",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("password", "Password must be of 5 characters").isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: "Please enter a unique value for email",
+          message: err.message,
+        });
+      });
+  }
+);
 
 module.exports = router;
