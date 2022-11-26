3795156e4109466601a91fa542ad25d5edfa0754
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-11-26 11:32:33 +0530

    Backend Auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,31 +5,42 @@ const { body, validationResult } = require("express-validator");
 
 // Create User, No Login Required
 router.post(
-  "/",
+  "/createuser",
   [
     body("email", "Enter a valid email").isEmail(),
     body("name", "Enter a valid name").isLength({ min: 3 }),
     body("password", "Password must be of 5 characters").isLength({ min: 5 }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    // If Errors, Return Bad Request And Errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    User.create({
+    // Check If The user with this email Exists Already
+    let user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      return res
+        .status(400)
+        .json({ error: "Sorry a user with this email already exists!" });
+    }
+
+    user = await User.create({
       name: req.body.name,
       password: req.body.password,
       email: req.body.email,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({
-          error: "Please enter a unique value for email",
-          message: err.message,
-        });
-      });
+    });
+    // .then((user) => res.json(user))
+    // .catch((err) => {
+    //   console.log(err);
+    //   res.json({
+    //     error: "Please enter a unique value for email",
+    //     message: err.message,
+    //   });
+    // });
+    res.json(user);
   }
 );
 
