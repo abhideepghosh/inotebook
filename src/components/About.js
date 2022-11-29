2a8cc8f446850184276f4223e3b60587a7822b0f
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2022-11-29 12:24:34 +0530

    ContextApi

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import NoteContext from "../context/notes/NoteContext";
 
 const About = () => {
-  return <div>This is About</div>;
+  const a = useContext(NoteContext);
+  useEffect(() => {
+    a.update();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div>
+      This is About {a.state.name} And He Belongs to the {a.state.class} class
+    </div>
+  );
 };
 
 export default About;
